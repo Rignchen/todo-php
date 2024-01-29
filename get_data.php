c1311a3658e2ba472941b9d9285e286482ef05cb
Rignchen <rinchendemort@gmail.com>
2024-01-29 15:25:42 +0100

    allow multiple request simultaniously

--- get_data.php
+++ get_data.php
@@ -5,15 +5,15 @@ if (isset($_POST["addTask"])) {
     $content[] = $_POST["addTask"];
     reload();
 }
-elseif (isset($_POST["removeTask"])) {
+if (isset($_POST["removeTask"])) {
     array_splice($content, $_POST["removeTask"], 1);
     reload();
 }
-elseif (isset($_POST["modifyTask"], $_POST["modifyTaskValue"])) {
+if (isset($_POST["modifyTask"], $_POST["modifyTaskValue"])) {
     $content[$_POST["modifyTask"]] = $_POST["modifyTaskValue"]; // I'm happy cause it worked on the first try
     reload();
 }
-elseif (isset($_GET["sort"])) {
+if (isset($_GET["sort"])) {
     if ($_GET["sort"] === "alpha") {
         natcasesort($content);
     }
@@ -22,7 +22,7 @@ elseif (isset($_GET["sort"])) {
         $content = array_reverse($content);
     }
 }
-elseif (isset($_GET["search"])) {
+if (isset($_GET["search"])) {
     $search = $_GET["search"];
     $content = array_filter($content, function ($task) use ($search) {
         if ($search === "") {
