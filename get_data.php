a41a9b920961a8c8095e6fcc2030deaec5b306e1
Rignchen <rinchendemort@gmail.com>
2024-01-29 14:46:09 +0100

    remove tasks

--- get_data.php
+++ get_data.php
@@ -5,3 +5,7 @@ if (isset($_POST["addTask"])) {
     $content[] = $_POST["addTask"];
     reload();
 }
+elseif (isset($_POST["removeTask"])) {
+    array_splice($content, $_POST["removeTask"], 1);
+    reload();
+}
--- task/task.php
+++ task/task.php
@@ -1,3 +1,6 @@
 <li>
-        <?= htmlspecialchars($task) ?>">
+    <form action="index.php" method="post">
+        <input type="hidden" name="removeTask" value="<?= $index ?>">
+        <button type="submit">X</button>
+    </form>
 </li>
