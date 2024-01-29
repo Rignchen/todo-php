4b7538a66960389f863d3ecfa4a083b5fb29c3d6
Rignchen <rinchendemort@gmail.com>
2024-01-29 14:53:09 +0100

    modify values

--- get_data.php
+++ get_data.php
@@ -9,3 +9,7 @@ elseif (isset($_POST["removeTask"])) {
     array_splice($content, $_POST["removeTask"], 1);
     reload();
 }
+elseif (isset($_POST["modifyTask"], $_POST["modifyTaskValue"])) {
+    $content[$_POST["modifyTask"]] = $_POST["modifyTaskValue"]; // I'm happy cause it worked on the first try
+    reload();
+}
--- task/task.php
+++ task/task.php
@@ -3,4 +3,9 @@
         <input type="hidden" name="removeTask" value="<?= $index ?>">
         <button type="submit">X</button>
     </form>
+    <form action="index.php" method="post">
+        <input type="hidden" name="modifyTask" value="<?= $index ?>">
+        <input type="text" name="modifyTaskValue" value="<?= htmlspecialchars($task) ?>">
+        <input type="submit">
+    </form>
 </li>
