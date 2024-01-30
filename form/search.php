2532be5fa587b21aebe5929320736a7a8d02dab1
Rignchen <rinchendemort@gmail.com>
2024-01-30 14:42:31 +0100

    let the user see his search

--- form/search.php
+++ form/search.php
@@ -1,6 +1,6 @@
 <form action="index.php" method="get">
     <label>
         <strong>Search </strong>
-        <input name="search" type="text">
+        <input name="search" type="text" value="<?= $_GET["search"] ?? "" ?>">
     </label>
 </form>
